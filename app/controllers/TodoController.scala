ce5697f418ba7fa6fe97a6f12ab32bc7c2fc4f3d
Fuma Ito <fmjp1012@gmail.com>
2024-05-15 16:27:42 +0900

    Add asynchronous results

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -14,17 +14,17 @@ import model.ViewValueHome
 
 import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class TodoController @Inject()(val controllerComponents: ControllerComponents, todoRepository: TodoRepository) extends BaseController {
 
-  def index() = Action { implicit req =>
+  def index() = Action.async { implicit req =>
     val vv = ViewValueHome(
       title  = "Todo",
       cssSrc = Seq("main.css"),
       jsSrc  = Seq("main.js")
     )
-    val todos = Await.result(todoRepository.get(), 5.seconds)
-    Ok(views.html.todo.Index(vv, todos))
+    todoRepository.getAll.map(todos => Ok(views.html.todo.Index(vv, todos)))
   }
 }
