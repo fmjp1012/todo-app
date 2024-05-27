bee82e0d4d50f0e152edd8d7fe22de25578add93
Fuma Ito <fmjp1012@gmail.com>
2024-05-27 14:25:47 +0900

    カテゴリ色にEnumStatusを導入

--- app/lib/model/TodoCategory.scala
+++ app/lib/model/TodoCategory.scala
@@ -6,6 +6,7 @@
 package lib.model
 
 import ixias.model._
+import ixias.util.EnumStatus
 
 import java.time.LocalDateTime
 
@@ -13,7 +14,7 @@ case class TodoCategory(
   id:        Option[TodoCategory.Id],
   name:      String,
   slug:      String,
-  color:     Short,
+  color:     TodoCategory.Color,
   updatedAt: LocalDateTime = NOW,
   createdAt: LocalDateTime = NOW
 ) extends EntityModel[TodoCategory.Id]
@@ -23,4 +24,17 @@ object TodoCategory {
   type Id = Long @@ TodoCategory
   type WithNoId = Entity.WithNoId[Id, TodoCategory]
   type EmbeddedId = Entity.EmbeddedId[Id, TodoCategory]
+
+  sealed abstract class Color(val code: Short, val name: String) extends EnumStatus
+  object Color extends EnumStatus.Of[Color] {
+    case object FRONTEND extends Color(code = 1, name = "frontend")
+    case object BACKEND  extends Color(code = 2, name = "backend")
+    case object INFRA    extends Color(code = 3, name = "infra")
+  }
+
+  def categoryColorToCssClass(color: Color): String = color match {
+    case Color.FRONTEND => "category-frontend"
+    case Color.BACKEND  => "category-backend"
+    case Color.INFRA    => "category-infra"
+  }
 }
--- app/lib/persistence/db/TodoCategoryTable.scala
+++ app/lib/persistence/db/TodoCategoryTable.scala
@@ -11,12 +11,12 @@ import lib.model.TodoCategory
 
 case class TodoCategoryTable(tag: Tag) extends Table[TodoCategory](tag, "to_do_category") {
   // Columns
-  /* @1 */ def id        = column[TodoCategory.Id] ("id",         UInt64, O.PrimaryKey, O.AutoInc)
-  /* @2 */ def name      = column[String]          ("name",       Utf8Char255)
-  /* @3 */ def slug      = column[String]          ("slug",       AsciiChar64)
-  /* @4 */ def color     = column[Short]           ("color",      UInt8)
-  /* @5 */ def updatedAt = column[LocalDateTime]   ("updated_at", TsCurrent)
-  /* @6 */ def createdAt = column[LocalDateTime]   ("created_at", Ts)
+  /* @1 */ def id        = column[TodoCategory.Id]    ("id",         UInt64, O.PrimaryKey, O.AutoInc)
+  /* @2 */ def name      = column[String]             ("name",       Utf8Char255)
+  /* @3 */ def slug      = column[String]             ("slug",       AsciiChar64)
+  /* @4 */ def color     = column[TodoCategory.Color] ("color",      UInt8)
+  /* @5 */ def updatedAt = column[LocalDateTime]      ("updated_at", TsCurrent)
+  /* @6 */ def createdAt = column[LocalDateTime]      ("created_at", Ts)
 
   // DB <=> Scala の相互のmapping定義
   def * = (id.?, name, slug, color, updatedAt, createdAt) <> (
