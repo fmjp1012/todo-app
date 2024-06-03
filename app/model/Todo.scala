8d2e7c67301c4b77f938a0f109e19a6a5768afe0
Fuma Ito <fmjp1012@gmail.com>
2024-06-03 14:55:25 +0900

    refactor /app structure

--- app/model/Todo.scala
+++ app/model/Todo.scala
@@ -1,4 +1,4 @@
-package lib.model
+package model
 
 import ixias.model._
 import ixias.util.EnumStatus
@@ -6,23 +6,23 @@ import ixias.util.EnumStatus
 import java.time.LocalDateTime
 
 case class Todo(
-  id:          Option[Todo.Id],
-  categoryId: TodoCategory.Id,
-  title:       String,
-  body:        String,
-  state:       Todo.Status,
-  updatedAt:   LocalDateTime = NOW,
-  createdAt:   LocalDateTime = NOW
+    id:         Option[Todo.Id],
+    categoryId: TodoCategory.Id,
+    title:      String,
+    body:       String,
+    state:      Todo.Status,
+    updatedAt:  LocalDateTime = NOW,
+    createdAt:  LocalDateTime = NOW
 ) extends EntityModel[Todo.Id]
 
 object Todo {
-  val  Id = the[Identity[Id]]
-  type Id = Long @@ Todo
-  type WithNoId = Entity.WithNoId [Id, Todo]
+  val Id = the[Identity[Id]]
+  type Id         = Long @@ Todo
+  type WithNoId   = Entity.WithNoId[Id, Todo]
   type EmbeddedId = Entity.EmbeddedId[Id, Todo]
 
   sealed abstract class Status(val code: Short, val name: String) extends EnumStatus
-  object Status extends EnumStatus.Of[Status] {
+  object Status                                                   extends EnumStatus.Of[Status] {
     case object IS_INACTIVE  extends Status(code = 0, name = "TODO")
     case object IS_ACTIVE    extends Status(code = 1, name = "進行中")
     case object IS_COMPLETED extends Status(code = 2, name = "完了")
--- app/model/TodoCategory.scala
+++ app/model/TodoCategory.scala
@@ -1,4 +1,4 @@
-package lib.model
+package model
 
 import ixias.model._
 import ixias.util.EnumStatus
@@ -6,24 +6,26 @@ import ixias.util.EnumStatus
 import java.time.LocalDateTime
 
 case class TodoCategory(
-  id:        Option[TodoCategory.Id],
-  name:      String,
-  slug:      String,
-  color:     TodoCategory.Color,
-  updatedAt: LocalDateTime = NOW,
-  createdAt: LocalDateTime = NOW
+    id:        Option[TodoCategory.Id],
+    name:      String,
+    slug:      String,
+    color:     TodoCategory.Color,
+    updatedAt: LocalDateTime = NOW,
+    createdAt: LocalDateTime = NOW
 ) extends EntityModel[TodoCategory.Id]
 
 object TodoCategory {
-  val  Id = the[Identity[Id]]
-  type Id = Long @@ TodoCategory
-  type WithNoId = Entity.WithNoId[Id, TodoCategory]
+  val Id = the[Identity[Id]]
+  type Id         = Long @@ TodoCategory
+  type WithNoId   = Entity.WithNoId[Id, TodoCategory]
   type EmbeddedId = Entity.EmbeddedId[Id, TodoCategory]
 
-  sealed abstract class Color(val code: Short) extends EnumStatus {
+  def exists(categoryId: Long): Boolean = ???
+
+  sealed abstract class Color(val code: Short) extends EnumStatus           {
     def toCssClass: String
   }
-  object Color extends EnumStatus.Of[Color] {
+  object Color                                 extends EnumStatus.Of[Color] {
     case object FRONTEND extends Color(code = 1) {
       override def toCssClass: String = "category-frontend"
     }
